51dfb002437d96f4320de9bcd813667c8e62223e
Reydel Ocon <reydel321@gmail.com>
2022-11-19 00:28:51 +0800

    hays

--- src/components/PaginationButton.jsx
+++ src/components/PaginationButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const PaginationButton = ({totalPost, countryPerPage, setCurrrentPage}) => {
 
@@ -8,11 +8,41 @@ const PaginationButton = ({totalPost, countryPerPage, setCurrrentPage}) => {
       countryIndex.push(i)
     }
 
+    const [windowSize, setWindowSize] = useState({
+      width: undefined
+    });
+
+    const [paginationDecider, setPaginationDeciderd] = useState(10)
+
+    // useEffect(() => {
+
+    //   const getCurrentWindowSize = () => {
+    //     setWindowSize({
+    //       width: window.innerWidth
+    //     })
+    //   }
+
+    // window.addEventListener("resize", getCurrentWindowSize)
+
+    // console.log(windowSize)
+
+    // if(windowSize < 1000){
+    //   setPaginationDeciderd(8)
+    // }
+
+    //   return () => {
+    //     window.addEventListener("resize", getCurrentWindowSize)
+    //   };
+
+    // }, [windowSize])
+
+    
+
   return (
     <div className='paginationBTN__container'>
 
         {countryIndex.map((country, index) => (
-              index < 10 &&      
+              index < paginationDecider &&      
               <div key={index} className='paginationButton'>
                 <button onClick={() => setCurrrentPage(country)}>{country}</button>
               </div>
